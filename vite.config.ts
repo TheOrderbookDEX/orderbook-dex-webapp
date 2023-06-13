d985b7720069805850036ce6033c32fe3f325f0c
Frugal-Wizard <108354301+Frugal-Wizard@users.noreply.github.com>
2023-06-13 10:53:03 -0300

    set up chunking in build options

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,18 @@ import eslint from 'vite-plugin-eslint';
 export default defineConfig({
   plugins: [ react(), eslint() ],
   base: '/app/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('@theorderbookdex')) return '@theorderbookdex';
+            if (id.includes('@ethersproject')) return '@ethersproject';
+            if (id.includes('apexcharts')) return 'apexcharts';
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 });
